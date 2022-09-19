e61d24482ff28f6439e1ccb71c8801b546664c30
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2022-09-19 14:24:57 +0900

    [Gold IV] Title: 알파벳, Time: 7644 ms, Memory: 116684 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,23 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def dfs(x, y):
+    global answer
+    stack = set([(x, y, words[x][y])])
+    while stack:
+        x, y, alp = stack.pop()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < R and 0 <= ny < C and words[nx][ny] not in alp:
+                stack.add((nx, ny, alp + words[nx][ny]))
+                answer = max(answer, len(alp) + 1)
+    return answer
+
+
+R, C = map(int, input().split())
+words = [list(map(str, input())) for _ in range(R)]
+answer = 1
+print(dfs(0, 0))
+
