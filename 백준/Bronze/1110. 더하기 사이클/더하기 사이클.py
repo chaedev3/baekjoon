dca95f9fd494665eba702a0dec4404913d727a62
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2022-09-19 14:33:33 +0900

    [Bronze I] Title: 더하기 사이클, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,15 @@
+n = int(input())
+num = n
+result = 0  
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = (b * 10) + c
+
+    result += 1 
+    if(num == n):
+        break
+
+print(result)  
