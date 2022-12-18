27fedd465df6f2cad11a03ab63412bc448d67e12
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2022-12-18 11:59:57 +0900

    [Silver I] Title: 물병, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -1,8 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 N, K = map(int, input().split())
 
 cnt = 0
 while bin(N).count('1') > K:
-    N += 1
-    cnt += 1
+    bottles = 1 << bin(N)[::-1].index('1')
+    cnt += bottles
+    N += bottles
 
 print(cnt)
