cb5d8fcf3f3dfa2d560d454d59e515ec1216b587
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2023-02-21 10:51:12 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,3 @@
 function solution(numbers) {
-    var answer = [];
-    for(let i = 0; i < numbers.length; i ++) {
-        answer.push(numbers[i] * 2)
-    }
-    return answer; 
+	 return numbers.map((number) => number * 2)  
 }
