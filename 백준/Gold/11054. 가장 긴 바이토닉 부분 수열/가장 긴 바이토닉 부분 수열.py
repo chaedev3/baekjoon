c7f202b03a46d6dbc68ad77a853a31c1b9c243d0
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2023-03-26 15:38:29 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 192 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -12,8 +12,6 @@ for i in range(N):
     for j in range(i):
         if numbers[i] > numbers[j] and dp_one[i] < dp_one[j]:
             dp_one[i] = dp_one[j] 
-        elif reverse_numbers[i] > reverse_numbers[j] and dp_two[i] < dp_two[j]:
-            dp_two[i] = dp_two[j] 
     dp_one[i] += 1 
 
 for i in range(N):
