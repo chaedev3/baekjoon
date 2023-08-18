41ec430f4643332a4bc02f494bd36ad668af9cad
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2023-08-18 16:35:59 +0900

    [Silver III] Title: 소수 구하기, Time: 304 ms, Memory: 41052 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,16 @@
-def isPrime(num):
-    if num == 1:
-        return False
-    else:
-        for i in range(2, int(num**0.5)+1):
-            if num % i == 0:
-                return False
-        return True
+import sys, math
+input = sys.stdin.readline 
 
+M, N = map(int, input().split()) 
 
-a, b = map(int, input().split())
-for i in range(a, b+1):
-    if isPrime(i):
-        print(i)
+array = [0] * (N + 1) 
+
+for i in range(2, int(math.sqrt(N)) + 1):
+    if array[i] == 0:
+        for j in range(i * 2, N + 1, i):
+            array[j] = 1 
+    
+
+for i in range(M, N + 1): 
+    if array[i] == 0  and i != 1: 
+        print(i) 
