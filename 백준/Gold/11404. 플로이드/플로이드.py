595aeb3410e1ea47d83640d32505d1d413f5efb4
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2024-10-22 19:14:30 +0900

    [Gold IV] Title: 플로이드, Time: 520 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,40 +1,31 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
-# n: 도시의 개수, m: 버스의 개수
+INF = int(1e9)
+
 n = int(input())
 m = int(input())
 
-graph = [[0] * n for _ in range(n)]
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
 for _ in range(m):
-    start, end, cost = map(int, input().split())
-    # 똑같은 루트인데 더 비용이 적은 경우가 있기 때문에 이렇게 처리해줌 
-    if 0 < graph[start - 1][end - 1] < cost:
+    a, b, c = map(int, input().split())
+    if graph[a][b] != INF and graph[a][b] < c: 
         pass
     else:
-        graph[start - 1][end - 1] = cost
+        graph[a][b] = c
 
-INF = float('inf')
+for i in range(1, n + 1):
+    for x in range(1, n + 1):
+        for y in range(1, n + 1):
+            graph[x][y] = min(graph[x][y], graph[x][i] + graph[i][y])
 
-result = [[INF] * n for _ in range(n)]
-# 출발지와 도착지가 같은 경우는 0, 비용이 있는 경우에는 그 비용으로 초기 세팅해줌 
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            result[i][j] = 0
-        elif graph[i][j] != 0:
-            result[i][j] = graph[i][j]
-
-# k를 중간에 거치는 루트로 해서 더 적은 비용으로 갱신해줌 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            result[i][j] = min(result[i][j], result[i][k] + result[k][j])
-
-# INF 로 처리된 부분을 0으로 바꿔주고 결과를 출력해줌 
-for i in range(n):
-    for j in range(n):
-        if result[i][j] == INF:
-            result[i][j] = 0
-        print(result[i][j], end=' ')
+for x in range(1, n + 1):
+    for y in range(1, n + 1):
+        if graph[x][y] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[x][y], end=' ')
     print()
-
