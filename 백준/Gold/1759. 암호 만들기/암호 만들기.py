c3f9b21259152760a270b17e7e63c70c16d81779
chaedev3 <109324466+chaedev3@users.noreply.github.com>
2024-10-28 17:23:57 +0900

    [Gold V] Title: 암호 만들기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,20 +1,20 @@
-from itertools import combinations
-L, C = map(int, input().split())
-# 알파벳 순으로 정렬하기
-words = list(map(str, input().split()))
-vowels = ['a', 'e', 'i', 'o', 'u']
+import sys, itertools
+input = sys.stdin.readline
+l, c = map(int, input().split())
+alphabet_list = list(map(str, input().split()))
+a_list = ['a', 'e', 'i', 'o', 'u'] 
 
-word = list(combinations(words, L))
-real_word = []
-for w in word:
-    cnt = 0
-    for idx in range(L):
-        if w[idx] in vowels:
-            cnt += 1
-    if 1 <= cnt <= (L - 2):
-        real_word.append(sorted(w))
+result = [] 
+for x in itertools.combinations(alphabet_list, l):
+    a_cnt = 0 
+    for a in a_list:
+        if a in list(x):
+            a_cnt += 1
 
-for ww in sorted(real_word):
-    print(''.join(ww))
+    if 1 <= a_cnt <= l - 2: 
+        result.append(''.join(sorted(list(x))))
 
+result.sort()
 
+for r in result:
+    print(r) 
